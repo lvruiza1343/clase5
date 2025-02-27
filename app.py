a72925798e6eb79507317c90997469d81beb046d
lvruiza1343 <lauraruizalvarez70@gmail.com>
2025-02-27 10:12:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-
+from PIL import Image 
 
 st.title("Mi primera app")
 
+st.header("en este espacio comienzo a desarrollar mis apliacaciones para interfaces multimodales")
+st.writer("me encantan los campos de lavanda.")
+image = Image.open('lavanda.jpeg')
 
-
-image
+st.image(image, caption = 'lavanda')
