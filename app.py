51ca8dbd37112ad5a3599fc5cb3f3389353d0be8
lvruiza1343 <lauraruizalvarez70@gmail.com>
2025-02-27 10:13:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi primera app")
 
 st.header("en este espacio comienzo a desarrollar mis apliacaciones para interfaces multimodales")
-st.writer("me encantan los campos de lavanda.")
+st.write("me encantan los campos de lavanda.")
 image = Image.open('lavanda.jpeg')
 
 st.image(image, caption = 'lavanda')
