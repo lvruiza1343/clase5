1a34db0a4533afe5dbd121fd4e4c63a9e2c1120e
lvruiza1343 <lauraruizalvarez70@gmail.com>
2025-02-27 10:47:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
 import streamlit as st
 from PIL import Image 
 
-st.title("Mi primera app")
 
-st.header("en este espacio comienzo a desarrollar mis apliacaciones para interfaces multimodales")
-st.write("me encantan los campos de lavanda.")
-image = Image.open('lavanda.jpeg')
+st.title("Hello word")
 
-st.image(image, caption = 'lavanda')
+st.header("En este espacio comienzo a desarrollar mis apps de interfaces multimodales")
+st.write("me enacnatna los campos de lavanda")
+image = Image.open("lavanda.jpeg")
 
+st.image(image, caption="Ay mi madre el bicho")
 
-texto = st.text_input('Escribe algo', 'Este es mi texto')
-st.write('El texto escrito es:', texto)
+texto = st.text_input("El mejor leon de la historia mundial", "este es unico y divno")
+st.write('El leon te ama',texto)
 
+st.subheader("Ahora hay dos columnas")
 
+col1,col2 = st.columns(2)
 
+with col1:
+  st.subheader("primera columna")
+  st.write("Todo es mejor si programando estoy")
+  respecto = st.checkbox("Estoy de acuerdo")
+  if respecto:
+    st.write("Buenisima")
 
-
-whit col1:
-st.subheader(Esta es la primera columna")
-ST.write("Las interfaces multimodales mejoran la experiencia de usuario")
-respecto = st.checbox('Estoy de acuerdo)
-if resp:
-                      st.write('Correcto')
-                      
-whit col2:
-                      st.subheader("Esta es la segunda columna")
-                      modo = st.radio ("que modalidad es la principal en tu interfaz", ('visual', 'auditiva'. 'tactil))
-                      if modo == 'visual':
-                          st.write('La vista es fundamentalpara tu interfaz')
-                      if modo == 'auditiva':
-                          st. write('la audicion es fundamental para tu interfaz')
-                      if modo == 'Tactil'
-                          st.write('El tacto es fundametal para tu interfaz')
+with col2:
+  st.subheader("Segunda columna")
+  modo = st.radio("Tu que prefieres backed o fronted",("backend","fronted","ninguna"))
+  if modo == "backend":
+    st.write("Eres de los mios siuuuuuu")
+  if modo == "fronted":
+    st.write("Eres una mierda gasss")
+  if modo == "ninguna":
+    st.write("Que estas haciendo aqui entonces")
